3744abc8c438da70b8952be5ace832da96e8742a
mediwateyash <diwateyash2004@gmail.com>
2024-11-08 14:02:50 +0530

    hero js

---
+++ script.js
@@ -0,0 +1,11 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showNextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    document.querySelector('.slider').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+// Set interval for automatic slider
+setInterval(showNextSlide, 3000);
+0
